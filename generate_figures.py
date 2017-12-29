5a385d05207602ecfe9088e5d526daa2a44651c8
James Wilmott <jpwilmott3@gmail.com>
2017-12-28 23:01:42 -0500

    created zoomed NT discrim and TSM figures for NDSEG

--- generate_figures.py
+++ generate_figures.py
@@ -10,8 +10,8 @@ import shelve #for database writing and reading
 
 #screen dimensions for the office ocmputer = (19.2,10.44)
 
-shelvepath =  '/Users/jameswilmott/Documents/Python/et_mt/data/'; #'/Users/james/Documents/Python/et_mt/data/'; #	
-savepath = '/Users/jameswilmott/Documents/Python/et_mt/figures/'; #'/Users/james/Documents/Python/et_mt/figures/' #
+shelvepath =  '/Users/james/Documents/Python/et_mt/data/'; #	'/Users/jameswilmott/Documents/Python/et_mt/data/'; #
+savepath = '/Users/james/Documents/Python/et_mt/figures/' #'/Users/jameswilmott/Documents/Python/et_mt/figures/'; #
 
 subject_data = shelve.open(shelvepath+'mt_data.db');
 db = subject_data; id = 'agg';
@@ -26,38 +26,38 @@ matplotlib.rcParams['hatch.linewidth'] = 9.0; #set the hatch width to larger tha
 matplotlib.rcParams['hatch.color'] = 'black';
 matplotlib.pyplot.rc('font',weight='bold');
 
-# 0.0 number of targets data
-#detection task first
-fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(350,800); ax1.set_yticks(arange(400,850,100)); ax1.set_xlim([0.7,2.5]); ax1.set_xticks([1,1.6,2.2]);
-ax1.set_ylabel('Response Time',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
-ax1.set_xticklabels(['Zero','One','Two']);
-colors=['darkgrey','steelblue','mediumpurple']; 
-#detection task first
-ax1.bar(1,db['%s_Detect_abs_mean_rt'%(id)],color=colors[0],width=0.4);
-ax1.errorbar(1,db['%s_Detect_abs_mean_rt'%(id)],yerr=[[db['%s_Detect_abs_rt_bs_sems'%(id)]],[db['%s_Detect_abs_rt_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(1.6,db['%s_Detect_st_mean_rt'%(id)],color=colors[1],width=0.4);
-ax1.errorbar(1.6,db['%s_Detect_st_mean_rt'%(id)],yerr=[[db['%s_Detect_st_rt_bs_sems'%(id)]],[db['%s_Detect_st_rt_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(2.2,db['%s_Detect_mt_mean_rt'%(id)],color=colors[2],width=0.4);
-ax1.errorbar(2.2,db['%s_Detect_mt_mean_rt'%(id)],yerr=[[db['%s_Detect_mt_rt_bs_sems'%(id)]],[db['%s_Detect_mt_rt_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
-ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
-ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
-#save the labeled figure as a .png	
-filename = 'exp2_detection_nt_labeled';
-savefig(savepath+filename+'.png',dpi=400);
-#then get rid of labels and save as a .eps
-labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; labels[2]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
-ax1.set_xticklabels(labels);
-ax1.set_yticklabels(['','','','','','','','','','','','','','']);
-ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_detection_nt';
-savefig(savepath+filename+'.eps',dpi=400);
-show();
+# # 0.0 number of targets data
+# #detection task first
+# fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
+# ax1.set_ylim(350,800); ax1.set_yticks(arange(400,850,100)); ax1.set_xlim([0.7,2.5]); ax1.set_xticks([1,1.6,2.2]);
+# ax1.set_ylabel('Response Time',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
+# ax1.set_xticklabels(['Zero','One','Two']);
+# colors=['darkgrey','steelblue','mediumpurple']; 
+# #detection task first
+# ax1.bar(1,db['%s_Detect_abs_mean_rt'%(id)],color=colors[0],width=0.4);
+# ax1.errorbar(1,db['%s_Detect_abs_mean_rt'%(id)],yerr=[[db['%s_Detect_abs_rt_bs_sems'%(id)]],[db['%s_Detect_abs_rt_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(1.6,db['%s_Detect_st_mean_rt'%(id)],color=colors[1],width=0.4);
+# ax1.errorbar(1.6,db['%s_Detect_st_mean_rt'%(id)],yerr=[[db['%s_Detect_st_rt_bs_sems'%(id)]],[db['%s_Detect_st_rt_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(2.2,db['%s_Detect_mt_mean_rt'%(id)],color=colors[2],width=0.4);
+# ax1.errorbar(2.2,db['%s_Detect_mt_mean_rt'%(id)],yerr=[[db['%s_Detect_mt_rt_bs_sems'%(id)]],[db['%s_Detect_mt_rt_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
+# ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
+# ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
+# #save the labeled figure as a .png	
+# filename = 'exp2_detection_nt_labeled';
+# savefig(savepath+filename+'.png',dpi=400);
+# #then get rid of labels and save as a .eps
+# labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; labels[2]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
+# ax1.set_xticklabels(labels);
+# ax1.set_yticklabels(['','','','','','','','','','','','','','']);
+# ax1.set_ylabel(''); ax1.set_xlabel('');
+# filename = 'exp2_detection_nt';
+# savefig(savepath+filename+'.eps',dpi=400);
+# show();
 
 # #discrimination plot
 fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(350,800); ax1.set_yticks(arange(400,850,100));  ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
+ax1.set_ylim(600,750); ax1.set_yticks(arange(650,751,50));  ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]); #
 ax1.set_ylabel('Response Time',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
 ax1.set_xticklabels(['One','Two']);
 colors=['steelblue','mediumpurple']; 
@@ -71,20 +71,20 @@ ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
 ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
 #save the labeled figure as a .png	
 filename = 'exp2_discrimination_nt_labeled';
-savefig(savepath+filename+'.png',dpi=400);
+#savefig(savepath+filename+'.png',dpi=400);
 #then get rid of labels and save as a .eps
 labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
 ax1.set_xticklabels(labels);
 ax1.set_yticklabels(['','','','','','','','','','','','','','']);
 ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_discrimination_nt';
+filename = 'exp2_discrimination_nt_ZOOMED';
 savefig(savepath+filename+'.eps',dpi=400);
 show();
 
 
 #Seperate out th target shapes match cases
 fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(350,800); ax1.set_yticks(arange(400,850,100)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
+ax1.set_ylim(600,750); ax1.set_yticks(arange(650,751,50)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]); #ax1.set_ylim(350,800);
 ax1.set_ylabel('Response Time',size=18); ax1.set_xlabel('Condition',size=18,labelpad=40);
 ax1.set_xticklabels(['Target Shapes Match', 'Target Shapes Mismatch']);
 colors=['indigo','orchid']; 
@@ -97,97 +97,97 @@ ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
 ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
 #save the labeled figure as a .png	
 filename = 'exp2_discrimination_tsm_labeled';
-savefig(savepath+filename+'.png',dpi=400);
+#savefig(savepath+filename+'.png',dpi=400);
 #then get rid of labels and save as a .eps
 labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
 ax1.set_xticklabels(labels);
 ax1.set_yticklabels(['','','','','','','','','','','','','','']);
 ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_discrimination_tsm';
+filename = 'exp2_discrimination_tsm_ZOOMED';
 savefig(savepath+filename+'.eps',dpi=400);
 show();
 
-
-######### Accuracy for number of targets data
-
-#detection task
-fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,2.5]); ax1.set_xticks([1,1.6,2.2]);
-ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
-ax1.set_xticklabels(['Zero','One','Two']);
-colors=['darkgrey','steelblue','mediumpurple']; 
-ax1.bar(1,db['%s_Detect_abs_pc'%(id)],color=colors[0],width=0.4);
-ax1.errorbar(1,db['%s_Detect_abs_pc'%(id)],yerr=[[db['%s_Detect_abs_pc_bs_sems'%(id)]],[db['%s_Detect_abs_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(1.6,db['%s_Detect_st_pc'%(id)],color=colors[1],width=0.4);
-ax1.errorbar(1.6,db['%s_Detect_st_pc'%(id)],yerr=[[db['%s_Detect_st_pc_bs_sems'%(id)]],[db['%s_Detect_st_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(2.2,db['%s_Detect_mt_pc'%(id)],color=colors[2],width=0.4);
-ax1.errorbar(2.2,db['%s_Detect_mt_pc'%(id)],yerr=[[db['%s_Detect_mt_pc_bs_sems'%(id)]],[db['%s_Detect_mt_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
-ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
-ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
-#save the labeled figure as a .png	
-filename = 'exp2_detection_nt_PC_labeled';
-savefig(savepath+filename+'.png',dpi=400);
-#then get rid of labels and save as a .eps
-labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; labels[2]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
-ax1.set_xticklabels(labels);
-ax1.set_yticklabels(['','','','','','','','','','','','','','']);
-ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_detection_nt_PC';
-savefig(savepath+filename+'.eps',dpi=400);
-show();
-
-
-#discrimination task
-fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
-ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
-ax1.set_xticklabels(['One','Two']);
-colors=['steelblue','mediumpurple']; 
-#do one target, two targets, and then target shapes match vs. not match
-ax1.bar(1,db['%s_Discrim_st_pc'%(id)],color=colors[0],width=0.25);
-ax1.errorbar(1,db['%s_Discrim_st_pc'%(id)],yerr=[[db['%s_Discrim_st_pc_bs_sems'%(id)]],[db['%s_Discrim_st_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(1.6,db['%s_Discrim_mt_pc'%(id)],color=colors[1],width=0.25);
-ax1.errorbar(1.6,db['%s_Discrim_mt_pc'%(id)],yerr=[[db['%s_Discrim_mt_pc_bs_sems'%(id)]],[db['%s_Discrim_mt_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
-ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
-ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
-#save the labeled figure as a .png	
-filename = 'exp2_discrimination_nt_PC_labeled';
-savefig(savepath+filename+'.png',dpi=400);
-#then get rid of labels and save as a .eps
-labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
-ax1.set_xticklabels(labels);
-ax1.set_yticklabels(['','','','','','','','','','','','','','']);
-ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_discrimination_nt_PC';
-savefig(savepath+filename+'.eps',dpi=400);
-show();
-
-#Seperate out th target shapes match cases
-fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
-ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
-ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Condition',size=18,labelpad=40);
-ax1.set_xticklabels(['Target Shapes Match', 'Target Shapes Mismatch']);
-colors=['indigo','orchid']; 
-ax1.bar(1,db['%s_Discrim_match_pc'%(id)],color=colors[0],width=0.25);
-ax1.errorbar(1,db['%s_Discrim_match_pc'%(id)],yerr=[[db['%s_Discrim_match_pc_bs_sems'%(id)]],[db['%s_Discrim_match_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.bar(1.6,db['%s_Discrim_no_match_pc'%(id)],color=colors[1],width=0.25);
-ax1.errorbar(1.6,db['%s_Discrim_no_match_pc'%(id)],yerr=[[db['%s_Discrim_no_match_pc_bs_sems'%(id)]],[db['%s_Discrim_no_match_pc_bs_sems'%(id)]]],color='black',lw=6.0);
-ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
-ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
-ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
-#save the labeled figure as a .png	
-filename = 'exp2_discrimination_tsm_PC_labeled';
-savefig(savepath+filename+'.png',dpi=400);
-#then get rid of labels and save as a .eps
-labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]='';#have to do this to center the x ticks on correct spot without incurring ticks at every spot
-ax1.set_xticklabels(labels);
-ax1.set_yticklabels(['','','','','','','','','','','','','','']);
-ax1.set_ylabel(''); ax1.set_xlabel('');
-filename = 'exp2_discrimination_tsm_PC';
-savefig(savepath+filename+'.eps',dpi=400);
-show();
+# 
+# ######### Accuracy for number of targets data
+# 
+# #detection task
+# fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
+# ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,2.5]); ax1.set_xticks([1,1.6,2.2]);
+# ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
+# ax1.set_xticklabels(['Zero','One','Two']);
+# colors=['darkgrey','steelblue','mediumpurple']; 
+# ax1.bar(1,db['%s_Detect_abs_pc'%(id)],color=colors[0],width=0.4);
+# ax1.errorbar(1,db['%s_Detect_abs_pc'%(id)],yerr=[[db['%s_Detect_abs_pc_bs_sems'%(id)]],[db['%s_Detect_abs_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(1.6,db['%s_Detect_st_pc'%(id)],color=colors[1],width=0.4);
+# ax1.errorbar(1.6,db['%s_Detect_st_pc'%(id)],yerr=[[db['%s_Detect_st_pc_bs_sems'%(id)]],[db['%s_Detect_st_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(2.2,db['%s_Detect_mt_pc'%(id)],color=colors[2],width=0.4);
+# ax1.errorbar(2.2,db['%s_Detect_mt_pc'%(id)],yerr=[[db['%s_Detect_mt_pc_bs_sems'%(id)]],[db['%s_Detect_mt_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
+# ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
+# ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
+# #save the labeled figure as a .png	
+# filename = 'exp2_detection_nt_PC_labeled';
+# savefig(savepath+filename+'.png',dpi=400);
+# #then get rid of labels and save as a .eps
+# labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; labels[2]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
+# ax1.set_xticklabels(labels);
+# ax1.set_yticklabels(['','','','','','','','','','','','','','']);
+# ax1.set_ylabel(''); ax1.set_xlabel('');
+# filename = 'exp2_detection_nt_PC';
+# savefig(savepath+filename+'.eps',dpi=400);
+# show();
+# 
+# 
+# #discrimination task
+# fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
+# ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
+# ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Number of Targets',size=18,labelpad=40);
+# ax1.set_xticklabels(['One','Two']);
+# colors=['steelblue','mediumpurple']; 
+# #do one target, two targets, and then target shapes match vs. not match
+# ax1.bar(1,db['%s_Discrim_st_pc'%(id)],color=colors[0],width=0.25);
+# ax1.errorbar(1,db['%s_Discrim_st_pc'%(id)],yerr=[[db['%s_Discrim_st_pc_bs_sems'%(id)]],[db['%s_Discrim_st_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(1.6,db['%s_Discrim_mt_pc'%(id)],color=colors[1],width=0.25);
+# ax1.errorbar(1.6,db['%s_Discrim_mt_pc'%(id)],yerr=[[db['%s_Discrim_mt_pc_bs_sems'%(id)]],[db['%s_Discrim_mt_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
+# ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
+# ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
+# #save the labeled figure as a .png	
+# filename = 'exp2_discrimination_nt_PC_labeled';
+# savefig(savepath+filename+'.png',dpi=400);
+# #then get rid of labels and save as a .eps
+# labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]=''; #have to do this to center the x ticks on correct spot without incurring ticks at every spot
+# ax1.set_xticklabels(labels);
+# ax1.set_yticklabels(['','','','','','','','','','','','','','']);
+# ax1.set_ylabel(''); ax1.set_xlabel('');
+# filename = 'exp2_discrimination_nt_PC';
+# savefig(savepath+filename+'.eps',dpi=400);
+# show();
+# 
+# #Seperate out th target shapes match cases
+# fig = figure(figsize = (12.8,7.64)); ax1=gca(); #grid(True);
+# ax1.set_ylim(0.75,1.00); ax1.set_yticks(arange(0.8,1.01,0.05)); ax1.set_xlim([0.7,1.9]); ax1.set_xticks([1,1.6]);
+# ax1.set_ylabel('Proportion Correct',size=18); ax1.set_xlabel('Condition',size=18,labelpad=40);
+# ax1.set_xticklabels(['Target Shapes Match', 'Target Shapes Mismatch']);
+# colors=['indigo','orchid']; 
+# ax1.bar(1,db['%s_Discrim_match_pc'%(id)],color=colors[0],width=0.25);
+# ax1.errorbar(1,db['%s_Discrim_match_pc'%(id)],yerr=[[db['%s_Discrim_match_pc_bs_sems'%(id)]],[db['%s_Discrim_match_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.bar(1.6,db['%s_Discrim_no_match_pc'%(id)],color=colors[1],width=0.25);
+# ax1.errorbar(1.6,db['%s_Discrim_no_match_pc'%(id)],yerr=[[db['%s_Discrim_no_match_pc_bs_sems'%(id)]],[db['%s_Discrim_no_match_pc_bs_sems'%(id)]]],color='black',lw=6.0);
+# ax1.spines['right'].set_visible(False); ax1.spines['top'].set_visible(False);
+# ax1.spines['bottom'].set_linewidth(2.0); ax1.spines['left'].set_linewidth(2.0);
+# ax1.yaxis.set_ticks_position('left'); ax1.xaxis.set_ticks_position('bottom');
+# #save the labeled figure as a .png	
+# filename = 'exp2_discrimination_tsm_PC_labeled';
+# savefig(savepath+filename+'.png',dpi=400);
+# #then get rid of labels and save as a .eps
+# labels = [item.get_text() for item in ax1.get_xticklabels()]; labels[0]=''; labels[1]='';#have to do this to center the x ticks on correct spot without incurring ticks at every spot
+# ax1.set_xticklabels(labels);
+# ax1.set_yticklabels(['','','','','','','','','','','','','','']);
+# ax1.set_ylabel(''); ax1.set_xlabel('');
+# filename = 'exp2_discrimination_tsm_PC';
+# savefig(savepath+filename+'.eps',dpi=400);
+# show();
 
 # ##############################################################################################################################
 #
